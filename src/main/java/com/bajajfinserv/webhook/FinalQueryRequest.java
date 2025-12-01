291f08f1f046894ce8a689823f6838212e2bc69a
sai-sandeep-k <143401156+sai-sandeep-k@users.noreply.github.com>
2025-12-01 17:27:59 +0530

    Update FinalQueryRequest.java

--- src/main/java/com/bajajfinserv/webhook/FinalQueryRequest.java
+++ src/main/java/com/bajajfinserv/webhook/FinalQueryRequest.java
@@ -1,1 +1,22 @@
+package com.bajajfinserv.webhook;
+
+public class FinalQueryRequest {
+
+    private String finalQuery;
+
+    public FinalQueryRequest() {
+    }
+
+    public FinalQueryRequest(String finalQuery) {
+        this.finalQuery = finalQuery;
+    }
+
+    public String getFinalQuery() {
+        return finalQuery;
+    }
+
+    public void setFinalQuery(String finalQuery) {
+        this.finalQuery = finalQuery;
+    }
+}
 
